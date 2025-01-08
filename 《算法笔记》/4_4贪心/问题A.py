954f07085349ff6177f7abfe81ea1cbbb2c25e29
WangZhao <2419102388@qq.com>
2025-01-08 10:41:54 +0800

    《算法笔记》4.4 问题A

--- 《算法笔记》/4_4贪心/问题A.py
+++ 《算法笔记》/4_4贪心/问题A.py
@@ -1,3 +1,10 @@
+"""
+《算法笔记》4.4 简单贪心 问题 A 看电视
+贪心策略：
+    将电视节目按照结束时间进行排序，每次选择结束时间最早的节目，这样保证有尽可能多的时间选择后续节目
+"""
+
+
 def max_programs(_intervals):
     # 贪心策略：将电视节目按照结束时间进行排序，每次选择结束时间最早的节目，这样保证有尽可能多的时间选择后续节目
     sorted_intervals = sorted(_intervals, key=lambda x: x[1])
@@ -11,6 +18,7 @@ def max_programs(_intervals):
             end_time = interval[1]
 
     return ans
+
 
 # 循环读取输入
 while True:
